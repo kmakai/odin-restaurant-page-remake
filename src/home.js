b07e35e8ca68e8200f19fd225d97cc3d5d975d0d
kmakai <makaikwag@gmail.com>
2022-07-12 18:44:25 -0400

    4 files modified, 1 added

--- src/home.js
+++ src/home.js
@@ -1,1 +1,17 @@
 const homeDiv = document.createElement('div');
+const disDiv = document.createElement('div');
+const imgDiv = document.createElement('div');
+const disc = document.createElement('p');
+const discTitle = document.createElement('h2');
+
+discTitle.textContent = "Welcome to Binx's Cafe";
+disc.textContent =
+  'the most cozy warm cafe in town. Come in to have yourself the best coffee and meet the cat binx.';
+disDiv.append(discTitle, disc);
+homeDiv.append(disDiv, imgDiv);
+
+homeDiv.className = "home"
+
+export default function loadHome() {
+  return homeDiv;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
-import header from "./header.js";
+import header from './header.js';
+import home from './home.js';
 
-
-document.querySelector("#content").appendChild(header());
+document.querySelector('#content').appendChild(header());
+document.querySelector('#content').appendChild(home());
