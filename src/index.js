a850ec7b2ce47c1a2acbc6bee1aba8cc9426bbad
kmakai <makaikwag@gmail.com>
2022-07-12 18:54:58 -0400

    4 files modified, menu module completed. basic styles added for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import header from './header.js';
 import home from './home.js';
+import menu from './menu.js';
 
 document.querySelector('#content').appendChild(header());
-document.querySelector('#content').appendChild(home());
+document.querySelector('#content').appendChild(menu());
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,6 @@
 function createMenuItem(Item, discription, cost) {
   const menuitem = document.createElement('div');
-  const item = document.createElement("h4");
+  const item = document.createElement('h3');
   item.textContent = Item;
   menuitem.append(item);
   const itemInfo = document.createElement('p');
@@ -13,17 +13,53 @@ function createMenuItem(Item, discription, cost) {
   return menuitem;
 }
 
-function createMenu() {
+export default function createMenu() {
   const menu = document.createElement('div');
-  const menuTitle = document.createElement('h1');
-  menuTitle.textContent = "Menu";
+  const menuTitle = document.createElement('h2');
+  menuTitle.textContent = 'Menu';
   menu.append(menuTitle);
-  menu.appendChild(createMenuItem("Coffee", "black coffee with sugar or your choice of sweetener. also available with lightener +$1.", "$6.99"));
-  menu.appendChild(createMenuItem("Latte", "Espresso with your choice of milk as well as flavoring", "$4.45"));
-  menu.appendChild(createMenuItem("Carmel Macchiato", "Carmel sauce & Vanilla syrup W/ Espresso and your choice of milk", "$4.95"));
-  menu.appendChild(createMenuItem("Pumpkin Spice Latte", "1/2 Pumpkin Pie 1/2 White Mocha W/ Espresso and your choice of milk", "$5.30"));
-  menu.appendChild(createMenuItem("Cafe Breve", "double espresso + 5oz./14oz. steamed half & half", "$5.45"));
-  menu.appendChild(createMenuItem("Melted Snowman (Affagato)", "scoop of vanilla ice cream + double espresso", "$7.50"));
-  menu.classList = "menuct";
+  menu.appendChild(
+    createMenuItem(
+      'Coffee',
+      'black coffee with sugar or your choice of sweetener. also available with lightener +$1.',
+      '$6.99'
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      'Latte',
+      'Espresso with your choice of milk as well as flavoring',
+      '$4.45'
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      'Carmel Macchiato',
+      'Carmel sauce & Vanilla syrup W/ Espresso and your choice of milk',
+      '$4.95'
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      'Pumpkin Spice Latte',
+      '1/2 Pumpkin Pie 1/2 White Mocha W/ Espresso and your choice of milk',
+      '$5.30'
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      'Cafe Breve',
+      'double espresso + 5oz./14oz. steamed half & half',
+      '$5.45'
+    )
+  );
+  menu.appendChild(
+    createMenuItem(
+      'Melted Snowman (Affagato)',
+      'scoop of vanilla ice cream + double espresso',
+      '$7.50'
+    )
+  );
+  menu.className = 'menu';
   return menu;
 }
