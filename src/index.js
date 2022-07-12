f88ca403ee127cfeedb329ddef3022e826eecc2d
kmakai <makaikwag@gmail.com>
2022-07-12 17:00:52 -0400

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
 import header from "./header.js";
 
+
+const img = new Image();
+img.src = "./images/welcome.png"
 document.querySelector("#content").appendChild(header());
+document.querySelector("#content").appendChild(img);
