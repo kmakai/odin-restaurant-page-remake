ed6fa2ecd4f1aceb60427ba4ae22a377226b486c
kmakai <makaikwag@gmail.com>
2022-07-13 16:29:25 -0400

    3 files modified

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import header from './header.js';
 import home from './home.js';
 import menu from './menu.js';
 import contacts from './contact.js';
+
 const content = document.querySelector('#content');
+const main = document.createElement('main');
 
-content.append(header());
+content.append(header(), main);
+main.append(home());
 
-const headerdiv = document.querySelector('.headr');
 const homelink = document.getElementById('homelink');
 const menulink = document.querySelector('#menulink');
 const contactlink = document.querySelector('#contactlink');
 
 homelink.addEventListener('click', () => {
-  console.log('clicked');
+  main.innerHTML = '';
+  main.append(home());
 });
 
 menulink.addEventListener('click', () => {
-  console.log('clicked');
+  main.innerHTML = '';
+  main.append(menu());
 });
 
 contactlink.addEventListener('click', () => {
-  console.log('clicked');
+  main.innerHTML = '';
+  main.append(contacts());
 });
