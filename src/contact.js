5cfee982c7e4c6e88a449a1f0819838c84877d5a
kmakai <makaikwag@gmail.com>
2024-02-12 21:20:54 -0500

    css updates

--- src/contact.js
+++ src/contact.js
@@ -1,39 +1,36 @@
 export default function createContact() {
-  const contacts = document.createElement('div');
-  const conTitle = document.createElement('h2');
+  const contacts = document.createElement("div");
+  const conTitle = document.createElement("h2");
   conTitle.textContent = "Contacts";
   contacts.append(conTitle);
 
-  const contactBinx = document.createElement('div');
+  const contactBinx = document.createElement("div");
   const binxImg = document.createElement("img");
   binxImg.src = "./images/binx.jpg";
   contactBinx.append(binxImg);
-  
-  const binxInfo = document.createElement('div');
-  binxInfo.className = "contact-info"
-  binxInfo.innerHTML =
-     `<p>Binx Makai</p> 
+
+  const binxInfo = document.createElement("div");
+  binxInfo.className = "contact-info";
+  binxInfo.innerHTML = `<p>Binx Makai</p> 
      <p>555-555-6666</p> 
      <p>Owner and Mananger of Binx Cafe.</p>`;
   contactBinx.append(binxInfo);
 
-  const contactBella = document.createElement('div');
+  const contactBella = document.createElement("div");
   const bellaImg = document.createElement("img");
   bellaImg.src = "./images/bella.png";
   contactBella.append(bellaImg);
- 
-  const bellaInfo = document.createElement('div');
-  bellaInfo.className = "contact-info"
-  bellaInfo.innerHTML =
-      `<p>Bella Makai</p> 
+
+  const bellaInfo = document.createElement("div");
+  bellaInfo.className = "contact-info";
+  bellaInfo.innerHTML = `<p>Bella Makai</p> 
       <p>555-555-6666</p>
       <p>Barista of Binx Cafe.</p>`;
   contactBella.append(bellaInfo);
 
-
   contacts.append(contactBinx);
   contacts.append(contactBella);
-  contacts.className = "contacts"
+  contacts.className = "contacts";
 
   return contacts;
 }
